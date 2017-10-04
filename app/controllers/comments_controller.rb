f062bed91f214d672bdad16d3d1589cb73b8a4a3
sbaxter95 <sbaxter95@outlook.com>
2017-10-04 17:44:07 +0100

    got index route for the posts working

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+   @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    new_post = Post.create(post_params)
+    new_post.save
+
+    redirect_to new_album
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+
+    redirect_to post
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+    redirect_to post_url
+  end
+
+  protected
+  def post_params
+    params.require(:post).permit(:title, :body, :author, :image, :category)
+  end
+
+end

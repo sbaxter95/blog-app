edf23d06ed470281c09ac3fee01faf3f7cc62624
sbaxter95 <sbaxter95@outlook.com>
2017-10-04 21:04:36 +0100

    can add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,21 +4,33 @@ class CommentsController < ApplicationController
     @comments = @post.comments
   end
 
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
   def show
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.find(params[:id])
   end
 
   def create
+    post = Post.find(params[:post_id])
+    post.comments.create(comment_params)
+    redirect_to post
   end
 
   def update
-  end
-
-  def new
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  protected
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :location, :email_address)
   end
 end

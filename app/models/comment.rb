6b1d47a45e8e275a1f1894da987bc30a1e85689b
sbaxter95 <sbaxter95@outlook.com>
2017-10-05 09:09:40 +0100

    added some styling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
 	validates :commenter, :body, :location, :email_address, presence: true
+	validates :commenter, length: {minimum: 4, maximum:30}
+	validates :location, length: {minimum: 4, maximum:30}
 end
